7316d69371e71f39a2917417aa7bfdaf2527f635
Hack Reactor Students <joshuawyatt123@gmail.com>
2014-07-02 16:29:28 -0700

    Create 'LinksController' that makes get request and retrieves links stored on server.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,25 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, Links, $http) {
+  //create a data property on $scope
+  $scope.data = {};
+
+  $scope.getLinks = function(){
+    $http.get('/api/links').success(function(links){
+      $scope.data.links = links;
+    });
+    // $http({
+    //   method: 'GET',
+    //   url: '/api/links'
+    // })
+    // .success(function(){
+    //   console.log('links.js, data------------------------> ');
+    // })
+    // .error(function(){
+    //   console.log('ERROR');
+    // });
+  };
+
+  $scope.getLinks();
+
 });
